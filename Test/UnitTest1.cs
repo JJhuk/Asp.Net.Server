8b1bf8cb128828e93f43862ee8ec9680ac977123
JJhuk <ly0738J@gmail.com>
2020-09-17 18:05:55 +0900

    set

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            var options = new DbContext();
+        }
+        
+        [Fact]
+        public void Test1()
+        {
+            //given
+            
+            //when
+            
+            //then
+        }
+    }
+}
