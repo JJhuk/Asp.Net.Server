c6a2e1392263c498a6508b94927e1823030c9fc0
JJhuk <ly0738J@gmail.com>
2020-09-28 19:35:24 +0900

    delete invalid test

--- Tests/Integration/IndexPageTests.cs
+++ Tests/Integration/IndexPageTests.cs
@@ -1,13 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Net;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Domain;
-using Domain.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Server.Dtos;
 using Wd3w.AspNetCore.EasyTesting;
-using Wd3w.AspNetCore.EasyTesting.Authentication;
 using Wd3w.AspNetCore.EasyTesting.EntityFrameworkCore;
 using Wd3w.AspNetCore.EasyTesting.Hestify;
 using Xunit;
@@ -35,34 +29,6 @@ namespace Tests.Integration
 
             // Then
             response.ShouldBeOk();
-        }
-
-        [Fact]
-        public async Task Hello2()
-        {
-            // Given 
-            var client = SUT.ReplaceSqliteInMemoryDbContext<UserContext>()
-                .SetupFixture<UserContext>(async context =>
-                {
-                    context.Users.Add(new User
-                    {
-                        Email = "test@test.com",
-                        Username = "test"
-                    });
-                    await context.SaveChangesAsync();
-                })
-                .AllowAuthentication(JwtBearerDefaults.AuthenticationScheme, new ClaimsIdentity(new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, "test")
-                }, "Bearer"))
-                .NoUserAuthentication()
-                .DenyAuthentication()
-                .CreateClient();
-
-            // When
-            var hello = await client.GetAsync("/Users");
-            // Then
-            hello.ShouldBe(HttpStatusCode.OK);
         }
     }
 }
